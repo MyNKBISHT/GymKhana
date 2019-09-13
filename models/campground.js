d9658038cb14f7af455d35fedf55c2bf40c71e49
Your Name <you@example.com>
2019-09-13 19:03:21 +0000

    add user profile

--- models/campground.js
+++ models/campground.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 var campgroundSchema = new mongoose.Schema({
    name: String,
-	price: String,
+   price: String,
    image: String,
    description: String,
    comments: [
--- routes/Auth.js
+++ routes/Auth.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
+var Campground =require("../models/campground");
 
 router.get("/", function(req, res){
 	res.render("landing");
@@ -12,7 +13,17 @@ router.get("/register", function(req, res){
 	res.render("register");
 });
 router.post("/register", function(req, res){
-	var newUser = new User({username: req.body.username});
+	var newUser = User(
+		{
+			username: req.body.username,
+			firstname: req.body.firstname,
+			lastname: req.body.lastname,
+			email: req.body.email,
+			avatar: req.body.avatar
+		});
+	if(req.body.admincode === "secretcode123"){
+		newUser.isAdmin = true;
+	}
 	User.register(newUser, req.body.password, function(err, user){
 		if(err){
 			req.flash("error", err.message);
@@ -41,5 +52,23 @@ router.get("/logout", function(req, res){
 	req.logout();
 	req.flash("success", "Logged you out !!!");
 	res.redirect("/campgrounds");
+});
+
+//USER Routes
+router.get("/users/:id", function(req, res){
+	User.findById(req.params.id, function(err, foundUser){
+		if(err){
+			req.flash("error", "Something Went Wrong!!!");
+			res.redirect("/");
+		}
+		Campground.find().where('author.id').equals(foundUser._id).exec(function(err, campgrounds){
+			if(err){
+			req.flash("error", "Something Went Wrong!!!");
+			res.redirect("/");
+		}else{
+			res.render("user/show", {user:foundUser, campgrounds:campgrounds});
+		}
+		});
+	});
 });
 module.exports = router;
